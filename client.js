7d3325e7b5cc2fecb87c211960b4c52fcbe424b9
amcabaraban <117131086+amcabaraban@users.noreply.github.com>
2026-02-14 23:03:31 +0800

    Update JSONBIN_API_KEY in client.js

--- client.js
+++ client.js
@@ -2,7 +2,7 @@
 // ================== CONFIGURATION ==================
 // REPLACE WITH YOUR ACTUAL CREDENTIALS
 const CONFIG = {
-    JSONBIN_API_KEY: '$2a$10$3mqRHpEXrn2wWNe1K2h4cuHjnlNzZP8HdJZeEvczm1LPTT/0nJoVK', // Your Master Key from Profile → API Keys
+    JSONBIN_API_KEY: '$2a$10$U8BrH5EcRobDpvnrqoD2UecdqTerHvqEMbH.xkf9IO8HYL3ICvhAC', // Your Master Key from Profile → API Keys
     BIN_ID: '699089c0d0ea881f40ba2fcf', // Your bin ID
     API_URL: 'https://api.jsonbin.io/v3',
     DEBUG: true
